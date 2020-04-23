345ddd09f9e941a3ee202c80aa6200651bd67844
terrytzh <terrytan@usc.edu>
2020-04-23 15:23:11 -0700

    Updated animation & ghost creation

--- Pacman.io/WebContent/client.js
+++ Pacman.io/WebContent/client.js
@@ -2,28 +2,44 @@ var Client = {};
 Client.socket = io('http://localhost:9092');
 
 Client.createNewPlayer = function() {
-	Client.socket.emit('newplayer', {message: 'newplayer'});
+    Client.socket.emit('newplayer', { message: 'newplayer' });
 }
 
 Client.updatePlayer = function(data) {
-	Client.socket.emit('playermovemnet', data);
+    Client.socket.emit('playermovemnet', data);
 }
 
-Client.socket.on('newplayer', function(player) {
-	game.addNewPlayer(player.x, player.y, player.id);
+Client.socket.on('newpacman', function(player) {
+    game.addNewPacman(player.x, player.y, player.id);
 })
 
-Client.socket.on('allplayers', function(data) {
-	for(var i = 0; i < data.length; i++){
-        game.addNewPlayer(data[i].x, data[i].y, data[i].id);
+Client.socket.on('newghost'),
+    function(player) {
+        game.addNewGhost(player.x, player.y, player.id);
     }
-	game.setCurrentPlayer(data[data.length-1].id);
+
+
+//allpacman or allghost is only called once upon initialization.
+Client.socket.on('allpacmans', function(data) {
+    for (var i = 0; i < data.length; i++) {
+        game.addNewPacman(data[i].x, data[i].y, data[i].id);
+    }
+    if (data.length > 0)
+        game.setCurrentPacman(data[data.length - 1].id);
+})
+
+Client.socket.on('allghosts', function(data) {
+    for (var i = 0; i < data.length; i++) {
+        game.addNewGhost(data[i].x, data[i].y, data[i].id);
+    }
+    if (data.length > 0)
+        game.setCurrentGhost(data[data.length - 1].id);
 })
 
 Client.socket.on('remove', function(id) {
-	game.removePlayer(id);
+    game.removePlayer(id);
 })
 
 Client.socket.on('playermovemnet', function(player) {
-	game.updatePlayer(player);
+    game.updatePlayer(player);
 })
