9936534de5904358cfd2a3c3a1204635408c9821
terrytzh <terrytan@usc.edu>
2020-04-26 01:13:43 -0700

    add ghostAI (staright pathfinding)
    Currently the ghost ais are working! Next thing is to implement the
    failsafes so that they won't get stuck if there's an invalid path found.

--- Pacman.io/WebContent/game.js
+++ Pacman.io/WebContent/game.js
@@ -20,8 +20,7 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
-
-
+var pathFinder;
 
 
 function preload() {
@@ -37,9 +36,6 @@ function preload() {
 
 
 function create() {
-	
-	
-	
     this.PACMAN_VELOCITY = 80;
     this.GHOST_VELOCITY = 75;
 
@@ -56,10 +52,17 @@ function create() {
     for(var y=0; y < this.map.height; y++){
     	var col = [];
     	for(var x=0; x < this.map.width; x++){
-    		
+    		var tile = this.map.getTileAt(x, y, true);
+    		if(tile == null)
+    			col.push(0);
+    		else
+    			col.push(tile.index);
     	}
+    	mapGrid.push(col);
     }
     
+    this.pathFinder.setGrid(mapGrid);
+    this.pathFinder.setAcceptableTiles([-1, 2, 3, 4, 5, 6]);
 
     var dotSprites = this.map.createFromTiles(2, null, {key: 'sprites', frame: 100}, this, this.cameras.main, layer='DotLayer');
     this.physics.world.enable(dotSprites);
@@ -78,8 +81,7 @@ function create() {
     Client.createNewPlayer();
 
     this.cursors = this.input.keyboard.createCursorKeys();
-    this.current_direction = Phaser.RIGHT;
-    this.select_direction = Phaser.RIGHT;
+    
     
 
     this.anims.create({
@@ -218,114 +220,187 @@ function create() {
 }
 
 function update() {
-    if (this.player) {
-    	//RODDUR CHANGE
-    	
-        var x = this.player.x;
-        var y = this.player.y;
-        var angle = this.player.angle;
-        if (this.player.oldPosition &&
-            (x != this.player.oldPosition.x ||
-                y != this.player.oldPosition.y ||
-                angle != this.player.oldPosition.angle)) {
+	if(this.player){
+		this.player.detectKeyboardInput();
+		this.player.update();
+	    if(this.player.type === 'ghost'){
+	    	for(let i in this.ghostMap){
+	    		if(this.ghostMap[i] != this.player){
+	    			this.ghostMap[i].calculatePath();
+	    			this.ghostMap[i].update();
+	    		}
+	    	}
+	    }
+	}
+    
+
+}
+
+function detectKeyboardInput() {
+	if (this.scene.cursors.left.isDown)
+        this.select_direction = Phaser.LEFT;
+    else if (this.scene.cursors.right.isDown)
+        this.select_direction = Phaser.RIGHT;
+    else if (this.scene.cursors.up.isDown)
+        this.select_direction = Phaser.UP;
+    else if (this.scene.cursors.down.isDown)
+        this.select_direction = Phaser.DOWN;
+}
+
+function calculatePath() {
+	var current_tile = this.scene.map.getTileAtWorldXY(this.getCenter().x, this.getCenter().y, true);
+	var destination_tile;
+	var that = this;
+	
+	//Gonna change if multiple pacmans are there
+	for(let index in this.scene.pacmanMap){
+		destination_tile = this.scene.map.getTileAtWorldXY(this.scene.pacmanMap[index].getCenter().x, this.scene.pacmanMap[index].getCenter().y, true);
+	}
+	
+	 
+	this.scene.pathFinder.findPath(current_tile.x, current_tile.y, destination_tile.x, destination_tile.y, function(path){
+		
+		if(path === null || path.length === 1){
+			//Failsafe: pick a random direction for now
+			
+			
+		}
+		else{
+			console.log(that.current_direction);
+			if(path[1].x === current_tile.x){
+				if(path[1].y > current_tile.y)
+					that.select_direction = Phaser.DOWN;
+				else if(path[1].y < current_tile.y)
+					that.select_direction = Phaser.UP;
+			}
+			if(path[1].y === current_tile.y){
+				if(path[1].x > current_tile.x)
+					that.select_direction = Phaser.RIGHT;
+				else if(path[1].x < current_tile.x)
+					that.select_direction = Phaser.LEFT;
+			}
+			if(that.select_direction + that.current_direction == 11 ||
+					that.select_direction + that.current_direction == 15){
+				that.select_direction += 1;
+				if(that.select_direction > 8)
+					that.select_direction = 5;
+			}
+			
+		}
+	})
+	this.scene.pathFinder.calculate();
+}
+
+function entityUpdate() {
+		
+        var x = this.x;
+        var y = this.y;
+        var angle = this.angle;
+        if (this.oldPosition &&
+            (x != this.oldPosition.x ||
+                y != this.oldPosition.y ||
+                angle != this.oldPosition.angle)) {
             Client.updatePlayer({
                 x: x,
                 y: y,
                 angle: angle,
-                id: this.player.id,
-                type: this.player.type,
-                animation: this.player.anims.getCurrentKey()
+                id: this.id,
+                type: this.type,
+                animation: this.anims.getCurrentKey()
             });
         }
 
-        this.player.oldPosition = {
-            x: this.player.x,
-            y: this.player.y,
-            angle: this.player.angle
+        this.oldPosition = {
+            x: this.x,
+            y: this.y,
+            angle: this.angle
         };
 
 
         //Detect key input, turning point & turn (set current_position to select_direction)
-        if(this.player.x > this.map.widthInPixels){
-        	this.player.setPosition(1, this.player.y);
+        if(this.x > this.scene.map.widthInPixels){
+        	this.setPosition(1, this.y);
         }
-        else if(this.player.x < 0){
-        	this.player.setPosition(this.map.widthInPixels-1, this.player.y);
+        else if(this.x < 0){
+        	this.setPosition(this.scene.map.widthInPixels-1, this.y);
         }
-        this.current_tile = this.map.getTileAtWorldXY(this.player.getCenter().x, this.player.getCenter().y, true);
-        this.directions[Phaser.LEFT] = this.map.getTileAt(this.current_tile.x - 1, this.current_tile.y, true);
-        this.directions[Phaser.RIGHT] = this.map.getTileAt(this.current_tile.x + 1, this.current_tile.y, true);
-        this.directions[Phaser.UP] = this.map.getTileAt(this.current_tile.x, this.current_tile.y - 1, true);
-        this.directions[Phaser.DOWN] = this.map.getTileAt(this.current_tile.x, this.current_tile.y + 1, true);
+        
+        this.current_tile = this.scene.map.getTileAtWorldXY(this.getCenter().x, this.getCenter().y, true);
+        this.directions[Phaser.LEFT] = this.scene.map.getTileAt(this.current_tile.x - 1, this.current_tile.y, true);
+        this.directions[Phaser.RIGHT] = this.scene.map.getTileAt(this.current_tile.x + 1, this.current_tile.y, true);
+        this.directions[Phaser.UP] = this.scene.map.getTileAt(this.current_tile.x, this.current_tile.y - 1, true);
+        this.directions[Phaser.DOWN] = this.scene.map.getTileAt(this.current_tile.x, this.current_tile.y + 1, true);
 
-        if (this.cursors.left.isDown)
-            this.select_direction = Phaser.LEFT;
-        else if (this.cursors.right.isDown)
-            this.select_direction = Phaser.RIGHT;
-        else if (this.cursors.up.isDown)
-            this.select_direction = Phaser.UP;
-        else if (this.cursors.down.isDown)
-            this.select_direction = Phaser.DOWN;
-
-        if (this.current_tile.index == 5 &&
-            this.select_direction != this.current_direction &&
-            this.select_direction + this.current_direction != 11 &&
-            this.select_direction + this.current_direction != 15 &&
-            this.directions[this.select_direction] != null &&
-            this.directions[this.select_direction].index != 1) {
-            if (Phaser.Math.Fuzzy.Equal(this.player.x, this.current_tile.getCenterX(), 1) && Phaser.Math.Fuzzy.Equal(this.player.y, this.current_tile.getCenterY(), 1)) {
-                this.current_direction = this.select_direction;
-                this.player.setPosition(this.current_tile.getCenterX(), this.current_tile.getCenterY());
-                this.player.body.reset(this.current_tile.getCenterX(), this.current_tile.getCenterY()); // Very Important! Reset all precalculated future positions to 0 to prevent update
-            }
-        }
+        
 
         //Update velocity according to current_direction
-        if (this.player.type == "pacman") {
+        if (this.type == "pacman") {
+        	if (this.current_tile.index == 5 &&
+                    this.select_direction != this.current_direction &&
+                    this.select_direction + this.current_direction != 11 &&
+                    this.select_direction + this.current_direction != 15 &&
+                    this.directions[this.select_direction] != null &&
+                    this.directions[this.select_direction].index != 1) {
+                    if (Phaser.Math.Fuzzy.Equal(this.x, this.current_tile.getCenterX(), 1) && Phaser.Math.Fuzzy.Equal(this.y, this.current_tile.getCenterY(), 1)) {
+                        this.current_direction = this.select_direction;
+                        this.setPosition(this.current_tile.getCenterX(), this.current_tile.getCenterY());
+                        this.body.reset(this.current_tile.getCenterX(), this.current_tile.getCenterY()); // Very Important! Reset all precalculated future positions to 0 to prevent update
+                    }
+                }
             if (this.current_direction == Phaser.LEFT) {
-                this.player.setVelocityY(0);
-                this.player.setVelocityX(-this.PACMAN_VELOCITY);
-                this.player.setAngle(180);
-                this.player.anims.play('pacman_move', true);
+                this.setVelocityY(0);
+                this.setVelocityX(-this.scene.PACMAN_VELOCITY);
+                this.setAngle(180);
+                this.anims.play('pacman_move', true);
             } else if (this.current_direction == Phaser.RIGHT) {
-                this.player.setVelocityY(0);
-                this.player.setVelocityX(this.PACMAN_VELOCITY);
-                this.player.setAngle(0);
-                this.player.anims.play('pacman_move', true);
+                this.setVelocityY(0);
+                this.setVelocityX(this.scene.PACMAN_VELOCITY);
+                this.setAngle(0);
+                this.anims.play('pacman_move', true);
             } else if (this.current_direction == Phaser.UP) {
-                this.player.setVelocityX(0);
-                this.player.setVelocityY(-this.PACMAN_VELOCITY);
-                this.player.setAngle(270);
-                this.player.anims.play('pacman_move', true);
+                this.setVelocityX(0);
+                this.setVelocityY(-this.scene.PACMAN_VELOCITY);
+                this.setAngle(270);
+                this.anims.play('pacman_move', true);
             } else if (this.current_direction == Phaser.DOWN) {
-                this.player.setVelocityX(0);
-                this.player.setVelocityY(this.PACMAN_VELOCITY);
-                this.player.setAngle(90);
-                this.player.anims.play('pacman_move', true);
+                this.setVelocityX(0);
+                this.setVelocityY(this.scene.PACMAN_VELOCITY);
+                this.setAngle(90);
+                this.anims.play('pacman_move', true);
             }
-        } else if (this.player.type == "ghost") {
+        } else if (this.type == "ghost") {
+        	if ((this.current_tile.index == 5 || this.current_tile.index == 6) &&
+                    this.select_direction != this.current_direction &&
+                    this.select_direction + this.current_direction != 11 &&
+                    this.select_direction + this.current_direction != 15 &&
+                    this.directions[this.select_direction] != null &&
+                    this.directions[this.select_direction].index != 1) {
+                    if (Phaser.Math.Fuzzy.Equal(this.x, this.current_tile.getCenterX(), 1) && Phaser.Math.Fuzzy.Equal(this.y, this.current_tile.getCenterY(), 1)) {
+                        this.current_direction = this.select_direction;
+                        this.setPosition(this.current_tile.getCenterX(), this.current_tile.getCenterY());
+                        this.body.reset(this.current_tile.getCenterX(), this.current_tile.getCenterY()); // Very Important! Reset all precalculated future positions to 0 to prevent update
+                    }
+                }
             if (this.current_direction == Phaser.LEFT) {
-                this.player.setVelocityY(0);
-                this.player.setVelocityX(-this.GHOST_VELOCITY);
-                this.player.anims.play('blinky_move_left', true);
+                this.setVelocityY(0);
+                this.setVelocityX(-this.scene.GHOST_VELOCITY);
+                this.anims.play(this.ghostType+'_move_left', true);
             } else if (this.current_direction == Phaser.RIGHT) {
-                this.player.setVelocityY(0);
-                this.player.setVelocityX(this.GHOST_VELOCITY);
-                this.player.anims.play('blinky_move_right', true);
+                this.setVelocityY(0);
+                this.setVelocityX(this.scene.GHOST_VELOCITY);
+                this.anims.play(this.ghostType+'_move_right', true);
             } else if (this.current_direction == Phaser.UP) {
-                this.player.setVelocityX(0);
-                this.player.setVelocityY(-this.GHOST_VELOCITY);
-                this.player.anims.play('blinky_move_up', true);
+                this.setVelocityX(0);
+                this.setVelocityY(-this.scene.GHOST_VELOCITY);
+                this.anims.play(this.ghostType+'_move_up', true);
             } else if (this.current_direction == Phaser.DOWN) {
-                this.player.setVelocityX(0);
-                this.player.setVelocityY(this.GHOST_VELOCITY);
-                this.player.anims.play('blinky_move_down', true);
+                this.setVelocityX(0);
+                this.setVelocityY(this.scene.GHOST_VELOCITY);
+                this.anims.play(this.ghostType+'_move_down', true);
             }
             
         }
-        this.physics.world.postUpdate();
-    }
-
+    
 }
 
 function eatDot(pacman, dot) {
@@ -341,57 +416,48 @@ function eatDot(pacman, dot) {
 
 }
 
-game.addNewPlayer = function(x, y, id) {
-	console.log(id);
-	this.scene.scenes[0].playerMap[id] = this.scene.scenes[0].physics.add.sprite(x, y, 'sprites');
-	this.scene.scenes[0].playerMap[id].body.setSize(16,16, true);
-	this.scene.scenes[0].playerMap[id].anims.play('pacman_move', true);
-	this.scene.scenes[0].physics.add.collider(this.scene.scenes[0].playerMap[id], this.scene.scenes[0].layer);
-}
-
-game.setCurrentPlayer = function(index) {
-	this.scene.scenes[0].player = this.scene.scenes[0].playerMap[index];
-	this.scene.scenes[0].player.id = index;
-}
-
-game.removePlayer = function(id) {
-	this.scene.scenes[0].playerMap[id].destroy();
-	delete this.scene.scenes[0].playerMap[id];
-}
-
-game.updatePlayer = function(otherPlayer) {
-	this.scene.scenes[0].playerMap[otherPlayer.id].x = otherPlayer.x;
-	this.scene.scenes[0].playerMap[otherPlayer.id].y = otherPlayer.y;
-	this.scene.scenes[0].playerMap[otherPlayer.id].angle = otherPlayer.angle;
-}
 
 game.addNewPacman = function(x, y, id) {
     console.log(id);
     this.scene.scenes[0].pacmanMap[id] = this.scene.scenes[0].physics.add.sprite(x, y, 'sprites');
+    this.scene.scenes[0].pacmanMap[id].type = 'pacman';
     this.scene.scenes[0].pacmanMap[id].body.setSize(16, 16, true);
     this.scene.scenes[0].pacmanMap[id].anims.play('pacman_move', true);
     this.scene.scenes[0].physics.add.collider(this.scene.scenes[0].pacmanMap[id], this.scene.scenes[0].layer);
+    this.scene.scenes[0].pacmanMap[id].current_direction = Phaser.RIGHT;
+    this.scene.scenes[0].pacmanMap[id].select_direction = Phaser.RIGHT;
+    this.scene.scenes[0].pacmanMap[id].update = entityUpdate;
+    this.scene.scenes[0].pacmanMap[id].processInput = detectKeyboardInput;
+    this.scene.scenes[0].pacmanMap[id].detectKeyboardInput = detectKeyboardInput;
+    this.scene.scenes[0].pacmanMap[id].directions = {};
 }
 
-game.addNewGhost = function(x, y, id) {
+game.addNewGhost = function(x, y, id, ghostType) {
     console.log(id);
     this.scene.scenes[0].ghostMap[id] = this.scene.scenes[0].physics.add.sprite(x, y, 'sprites');
+    this.scene.scenes[0].ghostMap[id].type = 'ghost';
+    this.scene.scenes[0].ghostMap[id].ghostType = ghostType;
     this.scene.scenes[0].ghostMap[id].body.setSize(16, 16, true);
-    this.scene.scenes[0].ghostMap[id].anims.play('blinky_move_right', true);
+    this.scene.scenes[0].ghostMap[id].anims.play(ghostType+'_move_right', true);
     this.scene.scenes[0].physics.add.collider(this.scene.scenes[0].ghostMap[id], this.scene.scenes[0].layer);
+    this.scene.scenes[0].ghostMap[id].current_direction = Phaser.UP;
+    this.scene.scenes[0].ghostMap[id].select_direction = Phaser.UP;
+    this.scene.scenes[0].ghostMap[id].update = entityUpdate;
+    this.scene.scenes[0].ghostMap[id].processInput = detectKeyboardInput;
+    this.scene.scenes[0].ghostMap[id].calculatePath = calculatePath;
+    this.scene.scenes[0].ghostMap[id].detectKeyboardInput = detectKeyboardInput;
+    this.scene.scenes[0].ghostMap[id].directions = {};
 }
 
 game.setCurrentPacman = function(index) {
     this.scene.scenes[0].player = this.scene.scenes[0].pacmanMap[index];
     this.scene.scenes[0].player.id = index;
-    this.scene.scenes[0].player.type = "pacman";
     this.scene.scenes[0].physics.add.overlap(this.scene.scenes[0].player, this.scene.scenes[0].dots, eatDot, null, this.scene.scenes[0]);
 }
 
 game.setCurrentGhost = function(index) {
     this.scene.scenes[0].player = this.scene.scenes[0].ghostMap[index];
     this.scene.scenes[0].player.id = index;
-    this.scene.scenes[0].player.type = "ghost";
 }
 
 game.removePacman = function(id) {
